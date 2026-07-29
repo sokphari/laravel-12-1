e7de848b8b5d0f0f230b75ee0946b1769ad7034d
petpina14022006-afk <petpina14022006@gmail.com>
2026-07-29 13:42:19 +0700

    update

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -52,7 +52,7 @@
         </a>
 
         <a
-            href=""
+            href="{{ url('/product') }}"
             @class([
                 'flex items-center gap-4 rounded-xl px-4 py-3 text-sm font-semibold transition',
                 'bg-blue-50 text-blue-600' => request()->routeIs('admin.products.*'),
@@ -77,7 +77,7 @@
         </a>
 
         <a
-            href="{{ url('/product') }}"
+            href=""
             @class([
                 'flex items-center gap-4 rounded-xl px-4 py-3 text-sm font-semibold transition',
                 'bg-blue-50 text-blue-600' => request()->routeIs('admin.categories.*'),
