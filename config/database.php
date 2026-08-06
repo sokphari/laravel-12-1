63e8064e37b3c0432c699c96857b85c88bc57e04
Nit <nit@Nits-MacBook-Air-2.local>
2026-08-06 12:52:01 +0700

    Auth

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
+use Pdo\Mysql;
 
 return [
 
@@ -59,8 +60,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                // \Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),​ php version 8.5
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'), //php version 8.2
+                \Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'), // PHP 8.5
+                // PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'), // PHP 8.2
             ]) : [],
         ],
 
